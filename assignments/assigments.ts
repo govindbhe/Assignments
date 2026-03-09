789237b3dba80c6d49181455c33a17b157f28596
Govind <govind@gmail.com>
2026-03-09 21:54:46 +0530

    assignments

--- assignments/assigments.ts
+++ assignments/assigments.ts
@@ -2,42 +2,45 @@
 
 
 // 1. Temperature of a city in degrees Celsius: 25.5 
-number
+let Temperature : number = 25.5;
 // 2. Whether a customer has placed an order: true or false
-boolean
+let customerOrderPlaced : boolean = true;
 // 3. Person's phone number: "123-456-7890"
-number
+let PhoneNumber:number=1234567890;
 // 4. Amount of money in a customer's bank account: 1000.50
-number
+let Amount: number = 1000.50;
 // 5. Person's email address: "john.doe@example.com"
-string
+let emailAddress: string = "john.doe@example.com"
 // 6. Coordinates of a location(latitude, longitude): 37.7749, -122.4194 
-number,number
+let location1: [number, number] = [37.7749, -122.4194];
 // 7. Person's marital status: true or false
-boolean
+let maritalstatus :boolean=true;
 // 8. Person's occupation: "Software Engineer"
-string
+let occupation: string = "Software Engineer";
 // 9. Person's favourite colour: "Blue"
-string
+let favouritecolour:string ="Blue"
 // 10.Current year: 2023
-date
+let currentyear: number = 2023;
+
 // 11.Number of followers on a social media platform: 1,000,000
-number
+let numberOfFollowers: number = 1000000;
+
 // 12.Rating of a movie: 7.5
-number
+let movieRating:number=7.5;
 // 13.Person's blood type: 'A'
-string
+let bloodType: string = 'A';
 // 14.Title of a book: "To Kill a Mockingbird"
-string
+let bookTitle: string = "To Kill a Mockingbird";
 // 15.Number of employees in a company: 500
-number
+let noOfEmpl:number=500;
 // 16.Time of an event: 2: 30 PM
-date
+let eventTime: string = "2:30 PM";
+
 // 17.Name of a country: "United States"
-string 
+let countryName: string = "United States";
 // 18.Person's eye color: "Brown"
-string
+let eyeColor: string = "Brown";
 // 19.Person's birthplace: "New York City"
-string
+let birthplace: string = "New York City";
 // 20. Distance between two cities: 200.5
-number
+let distanceBetweenCities: number = 200.5;
